56cad42d34aed65c2ac6f268f82f2388d3826883
Ku Lok Sun <sundayku199911@yahoo.com.hk>
2016-05-01 23:26:18 +0800

    Add command 'scrollToBottom'

--- test/specs/lifehack.js
+++ test/specs/lifehack.js
@@ -1,5 +1,20 @@
 var assert = require('assert');
 
+browser.addCommand("scrollToBottom", function (customVar) {
+    var height = this.getViewportSize('height');
+    return this.execute(
+        function(height){
+            var x = window.scrollX;
+            var y = window.scrollY;
+            do {
+                y += height;
+                window.scrollTo(x, y)
+            } while(y === window.scrollY);
+            return window.scrollY;
+        },
+        height
+    );
+});
 
 describe('lifehack home page', function() {
     it('should have Site Top 728 x 90 ad', function () {
