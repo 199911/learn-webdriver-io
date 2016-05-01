1387db25d4cd8bb072623ad2b344001f132bd835
Ku Lok Sun <sundayku199911@yahoo.com.hk>
2016-05-01 22:30:12 +0800

    Add site top ad

--- test/specs/lifehack.js
+++ test/specs/lifehack.js
@@ -1,21 +1,12 @@
 var assert = require('assert');
 
+
 describe('lifehack home page', function() {
-    it('should have google ads iframe', function () {
-        this.timeout(30000);
+    it('should have Site Top 728 x 90 ad', function () {
         browser
             .url('http://sunday.lifehack.org')
-            .pause(1000);
-
-        try {
-            console.log('Try frame with id');
-            console.log('before: ' + browser.isVisible('#google_ads_iframe_/1040155/SiteTop728x90_0'))
-            browser
-                .frame('google_ads_iframe_/1040155/SiteTop728x90_0')
-            console.log('after: ' + browser.isVisible('#google_ads_iframe_/1040155/SiteTop728x90_0'))
-        } catch (ex) {
-            console.log(ex);
-        }
-        
+            .waitForVisible('#google_ads_iframe_/1040155/SiteTop728x90_0',5000);
+        var res = browser.isVisible('#google_ads_iframe_/1040155/SiteTop728x90_0');
+        assert.ok(res);
     });
 });
